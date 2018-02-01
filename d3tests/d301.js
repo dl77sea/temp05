2c911d744bf97e1e3583aa44d738ac595b7f1ec8
dl77sea <danlarsson77@gmail.com>
2018-01-31 21:02:01 -0800

    multiline graph working

--- d3tests/d301.js
+++ d3tests/d301.js
@@ -1,71 +1,178 @@
 // set the dimensions and margins of the graph
 // var margin = {top: 20, right: 50, bottom: 30, left: 50},
-var margin = {top: 20, right: 20, bottom: 20, left: 50},
-    width = 160 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  },
+  width = 1000 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
 
 // parse the date / time
-// var parseTime = d3.timeParse("%d-%b-%y");
 var parseTime = d3.timeParse("%Y");
 // set the ranges
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
+// var threshX = d3.scaleTime().range([0, width]);
+// var threshY = d3.scaleLinear().range([height, 0]);
+
 // define the line
 var valueline = d3.line()
-    .x(function(d) { return x(d.snarf); })
-    .y(function(d) { return y(d.blarf); });
-
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-// var svg = d3.select("body").append("svg")
-var svg = d3.select("#content").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// Get the data
-// d3.csv("test00.csv", function(error, data) {
-d3.csv("test00.csv", function(error, data) {
-  // if (error) throw error;
-
-  // format the data
-  // data.forEach(function(d) {
-  //     d.snarf = parseTime(d.snarf);
-  //     console.log("parseTime from forEach: ", d.snarf)
-  //     d.blarf = +d.blarf;
-  // });
-
-  data = [{snarf: "2012", blarf: 3},{snarf: "2013", blarf: 5},{snarf: "2014", blarf: 12}]
-  data.forEach(function(d) {
-      d.snarf = parseTime(d.snarf);
-      console.log("parseTime from forEach: ", d.snarf)
-      // d.blarf = +d.blarf;
+  .x(function(d) {
+    return x(d.year);
+  })
+  .y(function(d) {
+    return y(d.val);
   });
 
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.snarf; }));
-  // y.domain([0, d3.max(data, function(d) { console.log("--",d); return d.blarf; })]);
-  y.domain(d3.extent(data, function(d) { console.log("--",d); return d.blarf; }));
-  console.log("this is the data: ", data)
-  // data = [{snarf: 2012, blarf: 3},{snarf: 2013, blarf: 5},{snarf: 2014, blarf: 12}]
-  console.log("this is the data: ", data)
-  // Add the valueline path.
-  svg.append("path")
+var svg = d3.select("#content").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+//get an array of objects where
+//each obj in arr should be like:
+//  {year: "2012", val: "3.0"}
+//per value line
+
+// Get the data
+// d3.csv("test01.csv", function(error, data) {
+//   if (error) throw error;
+
+var gMinMax;
+var gThresh = 1
+var gPadding = 0.05
+
+d3.csv("ratio00.csv", function(error, data) {
+  if (error) throw error;
+
+  //for each object, make it an array as above for each value line
+  let valueLines = []
+  for (valueLineObj of data) {
+    let valueLine = []
+    for (key in valueLineObj) {
+      if (key !== "") valueLine.push({
+        year: key,
+        val: valueLineObj[key]
+      })
+    }
+    valueLines.push(valueLine)
+  }
+
+  // data = valueLines[0]
+
+  colorInc = 0;
+  colors = ["line3"]
+  for (data of valueLines) {
+
+  // data = valueLines[0]
+
+    // format the data
+    data.forEach(function(d) {
+      // d.year = parseTime(d.year.toString());
+      d.year = parseTime(d.year);
+      d.val = parseFloat(d.val);
+    });
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) {
+      return d.year;
+    }));
+
+    const minMax = d3.extent(data, function(d) {
+      return d.val;
+    })
+
+    gMinMax = d3.extent(data, function(d) {
+      return d.year;
+    })
+
+    // y.domain([minMax[0] - gPadding, minMax[1] + gPadding]);
+    y.domain([0.65, 1.25]);
+    // Add the valueline path.
+    if(colorInc === 2) {
+      colorClass="line3"
+      console.log("testLine: ",data)
+
+    } else {
+      colorClass="line"
+    }
+    colorInc++
+    svg.append("path")
       .data([data])
-      .attr("class", "line")
+      .attr("class", colorClass)
       .attr("d", valueline);
+  }
+
+  // svg.append("path")
+  //     .attr("class", "line")
+  //     .attr("d", function(d) { return line(d.values); })
+  //     .style("stroke", function(d) { return z(d.id); });
+
+
+  // Add thereshold line
+  svg.append("line")
+    .attr("class", "line")
+    .attr("x1", x(gMinMax[0]))
+    .attr("y1", y(gThresh))
+    .attr("x2", x(gMinMax[1]))
+    .attr("y2", y(gThresh))
 
   // Add the X Axis
   svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(1));
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(20));
 
   // Add the Y Axis
   svg.append("g")
-      .call(d3.axisLeft(y).ticks(2));
-
+    .call(d3.axisLeft(y).ticks(20));
 });
+
+//mean line
+
+let meanLine = []
+d3.csv("ratiomean.csv", function(error, data) {
+  if (error) throw error;
+
+  //build mean line
+  for (obj of data) {
+    meanLine.push({
+      year: obj[""],
+      val: obj[0]
+    })
+  }
+  //format values in valueLine
+  for (obj of meanLine) {
+    obj.year = parseTime(obj.year)
+    obj.val = parseFloat(obj.val)
+  }
+
+
+  //plot mean line
+  data = meanLine
+  console.log("mean linee: ", data)
+
+  // Add the valueline path.
+  svg.append("path")
+    .data([data])
+    .attr("class", "line2")
+    .attr("d", valueline);
+
+  // for (data of meanLine) {
+  // Scale the range of the data
+  // x.domain(d3.extent(data, function(d) {
+  //   return d.year;
+  // }));
+  //
+  // let minMax = d3.extent(data, function(d) {
+  //   return d.val;
+  // })
+  //
+  // y.domain([minMax[0] - gPadding, minMax[1] + gPadding]);
+
+
+})
--- d3tests/d302.js
+++ d3tests/d302.js
@@ -4,36 +4,92 @@ var svg = d3.select("svg"),
     height = svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var parseTime = d3.timeParse("%Y%m%d");
+// var parseTime = d3.timeParse("%Y%m%d");
+var parseTime = d3.timeParse("%Y");
 
+//create functions for fitting data onto 2D plane
 var x = d3.scaleTime().range([0, width]),
     y = d3.scaleLinear().range([height, 0]),
     z = d3.scaleOrdinal(d3.schemeCategory10);
 
+    console.log("x: ", x)
+    console.log("y: ", y)
+    console.log("z: ", z)
+
+//make a line object (empty for now) to later hold (be deformed by) values from csv values
 var line = d3.line()
-    .curve(d3.curveBasis)
+    // .curve(d3.curveBasis)
+    // .x(function(d) { console.log(d.date); return x(d.date); })
+    // .y(function(d) { console.log(d.temperature); return y(d.temperature); });
     .x(function(d) { return x(d.date); })
+    // console.log("---",d.temperature); 
     .y(function(d) { return y(d.temperature); });
+    // .y(function(d) { return y(1); });
+    // console.log("line: ",line)
 
-d3.tsv("data.tsv", type, function(error, data) {
-  if (error) throw error;
+    // d3.tsv("data.tsv", type, function(error, data) {
+    //   if (error) throw error;
+    //
+    //   var cities = data.columns.slice(1).map(function(id) {
+    //     return {
+    //       id: id,
+    //       values: data.map(function(d) {
+    //         return {date: d.date, temperature: d[id]};
+    //       })
+    //     };
+    //   });
 
-  var cities = data.columns.slice(1).map(function(id) {
-    return {
-      id: id,
-      values: data.map(function(d) {
-        return {date: d.date, temperature: d[id]};
-      })
-    };
-  });
 
+    // d3.csv("ratio.csv", type, function(error, data) {
+    d3.csv("ratio.csv", function(error, data) {
+      if (error) throw error;
+
+    // like this:
+    //   [
+    //     //row objects
+    //     { id: row-first-element,
+    //       values: [ {year: flow-val}... ]
+    //     }...
+    //   ]
+
+  //get id: to be first element in row
+  //get values: to be year: flow-val
+
+  var checkVal = "0.05"
+  var valueLines = []
+
+  for(obj of data) {
+    let valueLine = {id: "f", values: []}
+    for(key in obj) {
+      if(obj[key] !== checkVal && obj[key] !== null) {
+        // valueLine.values.push({date: new Date(parseInt(key), 1), temperature: obj[key]})
+        // console.log(key)
+        // console.log(parseTime(key))
+
+        valueLine.values.push({date: parseTime(key), temperature: parseFloat(obj[key])})
+        //  d.snarf = parseTime(d.snarf.toString());
+        // valueLine.values.push({date: key, temperature: obj[key]})
+        // console.log()
+      }
+    }
+    valueLines.push(valueLine)
+  }
+  // console.log("valueLines[0] ",valueLines[0])
+
+  cities = valueLines;
+
+  console.log(cities)
+
+  //set range of dates along x axis to entire range of dates in csv
   x.domain(d3.extent(data, function(d) { return d.date; }));
 
+  //set range of values (flow ratio?) along y axis to whole range in csv per
   y.domain([
     d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
     d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
   ]);
 
+  //generate each year's value line
   z.domain(cities.map(function(c) { return c.id; }));
 
   g.append("g")
@@ -54,23 +110,28 @@ d3.tsv("data.tsv", type, function(error, data) {
   var city = g.selectAll(".city")
     .data(cities)
     .enter().append("g")
-      .attr("class", "city");
+    .attr("class", "city");
 
+  // console.log(city.attr("d", function(d) { return line(d.values); }))
   city.append("path")
       .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return z(d.id); });
 
-  city.append("text")
-      .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-      .attr("x", 3)
-      .attr("dy", "0.35em")
-      .style("font", "10px sans-serif")
-      .text(function(d) { return d.id; });
+      // .attr("d", function(d) { console.log(line(d.values)); return line(d.values); })
+      .attr("d", function(d) { return line(d.values); })
+      // .style("stroke", function(d) { return z(d.id); });
+
+  // city.append("text")
+  //     .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+  //     .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+  //     .attr("x", 3)
+  //     .attr("dy", "0.35em")
+  //     .style("font", "10px sans-serif")
+  //     .text(function(d) { return d.id; });
+
 });
 
 function type(d, _, columns) {
+  // console.log("-----",d)
   d.date = parseTime(d.date);
   for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
   return d;
